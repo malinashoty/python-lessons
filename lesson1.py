07c2c2bc033f6ac178ad9e05aab0b0f9797fd94c
lovla <malinashoty@gmail.com>
2024-01-15 04:51:16 +0300

    lesson 1

---
+++ lesson1.py
@@ -0,0 +1,6 @@
+years_old_of_Laura = 22.8
+years_old_of_Alex = 24
+
+age_difference = years_old_of_Alex-years_old_of_Laura
+
+print(age_difference, type(years_old_of_Laura), type(years_old_of_Alex))
